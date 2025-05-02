84244cf86a45a271cb30c38c24b49e31f4f42d5c
TatyanaShakhmina <novichkovataty@gmail.com>
2025-05-02 15:39:16 +0400

    Добавлен готовый проект

---
+++ __tests__/index.test.js
@@ -0,0 +1,14 @@
+import { getHealthStatus } from '../src/index.js';
+
+test('"healthy" for health > 50', () => {
+    expect(getHealthStatus({ name: 'Маг', health: 90 })).toBe('healthy');
+});
+
+test('"wounded" for 15 <= health <= 50', () => {
+    expect(getHealthStatus({ name: 'Tim', health: 50 })).toBe('wounded');
+    expect(getHealthStatus({ name: 'Boy', health: 15 })).toBe('wounded');
+});
+
+test('"critical" for health < 15', () => {
+    expect(getHealthStatus({ name: 'Artur', health: 10 })).toBe('critical');
+});
